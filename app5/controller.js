7890b7eb8ef7971e8af38e59aa1f8ff9a09f8ec4
Lloyd Grubham <grubham@gmail.com>
2017-04-27 15:48:17 -0600

    Completed game, need to fix html special characters.

--- app5/controller.js
+++ app5/controller.js
@@ -1,26 +1,31 @@
 angular.module('app').controller('mainCtrl', function ($scope, mainService) {
-    $scope.getTrivia = mainService.getTrivia().then(function (response) {
-        $scope.trivia = response.data.results;
-        $scope.right = 0;
-        $scope.wrong = 0;
-        $scope.q = 0;
-        $scope.done = false;
-        $scope.message = "Fix this!"
-        console.log(typeof $scope.trivia);
-        for (let i = 0; i < $scope.trivia.lenght; i++){ 
-            $scope.trivia[i].incorrect_answers.push($scope.trivia[i].correct_answer);
+    
+    $scope.getTrivia = function () {
+        mainService.getTrivia().then(function (response) {
+                $scope.trivia = response.data.results;
+                $scope.right = 0;
+                $scope.wrong = 0;
+                $scope.q = 0;
+                $scope.done = false;
+                $scope.message = "Fix this!"
+                console.log(typeof $scope.trivia);
+                for (let i = 0; i < $scope.trivia.length; i++) {
+                    $scope.trivia[i].incorrect_answers.push($scope.trivia[i].correct_answer);
+                    $scope.trivia[i].incorrect_answers.sort();
+                }
+            })
         }
-    });
+
     $scope.guess = function (answer) {
-        if($scope.trivia[$scope.q].incorrect_answers.indexOf(answer) == -1){
+        if ($scope.trivia[$scope.q].correct_answer === answer) {
             $scope.right++;
         } else {
             $scope.wrong++;
         }
         $scope.q++;
-        if ($scope.q == 10){
+        if ($scope.q == 10) {
             $scope.done = true;
-            if ($scope.right>$scope.wrong) {
+            if ($scope.right > $scope.wrong) {
                 $scope.message = "You Win!";
             } else if ($scope.right < $scope.wrong) {
                 $scope.message = 'You lose.';
@@ -30,5 +35,9 @@ angular.module('app').controller('mainCtrl', function ($scope, mainService) {
         }
     }
 
+    $scope.playAgain = function () {
+        $scope.getTrivia();
+    }
+    $scope.getTrivia();
 
 })
